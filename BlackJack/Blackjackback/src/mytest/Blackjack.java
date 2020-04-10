4558a55bc6b7cf4649772f0c0b9dbbee67f57d1a
Ben <61331798+b3nb4ggs@users.noreply.github.com>
2020-04-10 12:41:48 -0500

    Add files via upload

--- BlackJack/Blackjackback/src/mytest/Blackjack.java
+++ BlackJack/Blackjackback/src/mytest/Blackjack.java
@@ -14,13 +14,15 @@ int Dvalue2;
 int Pvalue2;
 Scanner in = new Scanner(System.in);
 
-
+//The black jack object has 2 hands of cards as ArrayLists and their associated value.
 public Blackjack() {
 	this.Dvalue=0;
 	this.Pvalue=0;
 	Dhand= new ArrayList<Card>();
 	Phand= new ArrayList<Card>();
 }
+
+//Deals the deck correctly alternating player and dealer.
 public Deck Deal(Deck a) {
 Dhand.add(a.DealCard());
 Phand.add(a.DealCard());
@@ -29,14 +31,29 @@ Phand.add(a.DealCard());
 return a;
 }
 
-public void HitorStay(Deck b) {
+//HitorStay method takes keyboard input and decides which method to move forward with.  
+//Eventually this will be JavaFx buttons
+public boolean HitorStay(Deck b) {
+	boolean stay = false;
 	System.out.println("Hit=H Stay=S");
 	String a = in.nextLine();
-	if(a.equals("H")||a.equals("h")) {PlayerHit(b);}
-	else if(a.equals("S")||a.equals("s")) {DealerTurn(b);}
-	else {System.out.println("Please input H or S");}
+	if(a.equals("H")||a.equals("h")) {
+		
+		PlayerHit(b);
+		return stay;}
+	else if(a.equals("S")||a.equals("s")) {
+		stay=true;
+		DealerTurn(b);
+		return stay;}
+	else {
+		System.out.println("Please input H or S");
+		return stay;}
 }
 
+//This is one of the main methods of the game.  Aces are worth 1 or 11 so we have a boolean representing if an ace showed up.
+//the method takes face cards and makes them worth 10.  It counts the totals and displays the result.
+//It shows if the player busts.  Eventually most of the business of the blackjack game will be in this method.
+//As of now it's still a work in progress.
 public String show() {
 	String result ="";
 	boolean DAce=false;
@@ -77,9 +94,9 @@ public String show() {
 return result;
 }
 
+//When the player chooses to hit it deals a card and goes to hit or stay. .
 public Deck PlayerHit(Deck a) {
 	Phand.add(a.DealCard());
-	HitorStay(a);
 	return a;
 
 }
--- BlackJack/Blackjackback/src/mytest/Card.java
+++ BlackJack/Blackjackback/src/mytest/Card.java
@@ -6,6 +6,9 @@ private int value;
 private String suitName;
 private String cardName;
 
+
+//The card class is made so that two ints designate the card for example (0,1)= ace of spades
+//This will help when we have a bunch of gif files to link to them and allows a for loop to make a deck.
 public Card(int s, int v) {
 	this.suit=s;
 	this.value=v;
@@ -13,6 +16,10 @@ public Card(int s, int v) {
 	this.cardName="";
 }
 
+
+//Notice that suits are 0-3 and values are 1-13.  The values start at 1 so I don't have to shift them all when adding
+//A total value for the cards.  
+//The result of make name is a string like "Ace of Spades"
 public String makeName(int s, int v) {
 	int dasuit=s;
 	int davalue=v;
@@ -63,15 +70,17 @@ public String makeName(int s, int v) {
 	return result;
 }
 	
-	
+//for totaling the cards in blackjack
 public int getValue() {
 	return value;
 }
 
+//setValue will never be used.
 public void setValue(int value) {
 	this.value = value;
 }
 
+//ToString just calls makeName() method.
 public String toString() {
 String result="";
 result = makeName(suit,value);
